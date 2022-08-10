92b7fef42a3b772a8580b81ca3f2b07cdad15d0f
Racim CHEKIRINE <racim.chekirine@edu.esiee.fr>
2022-08-10 18:00:52 +0200

    Adding tests checking validity of a Meal

--- backend-core/src/test/java/com/glady/service/ControlChequesTest.java
+++ backend-core/src/test/java/com/glady/service/ControlChequesTest.java
@@ -1,6 +1,7 @@
 package com.glady.service;
 
 import com.glady.entity.Gift;
+import com.glady.entity.Meal;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -49,5 +50,61 @@ public class ControlChequesTest {
 
         //then
         assertThat(gift.isValid()).isTrue();
+    }
+
+    @Test
+    void meal_becomes_invalid_after_expiration_date() {
+        //given
+        ControlCheques service = new ControlCheques();
+        Meal meal = new Meal(100, LocalDate.of(2022, 8, 10));
+
+        //when
+        LocalDate dateAfterExpirationDate = LocalDate.of(2023, 6, 10);
+        service.updateChequeValidity(meal, dateAfterExpirationDate);
+
+        //then
+        assertThat(meal.isValid()).isFalse();
+    }
+
+    @Test
+    void meal_is_still_valid_before_expiration_date() {
+        //given
+        ControlCheques service = new ControlCheques();
+        Meal meal = new Meal(100, LocalDate.of(2022, 8, 10));
+
+        //when
+        LocalDate dateBeforeExpirationDate = LocalDate.of(2023, 1, 10);
+        service.updateChequeValidity(meal, dateBeforeExpirationDate);
+
+        //then
+        assertThat(meal.isValid()).isTrue();
+    }
+
+    @Test
+    void meal_is_still_valid_the_day_of_the_expiration_date_when_leap_year() {
+        //given
+        ControlCheques service = new ControlCheques();
+        Meal meal = new Meal(100, LocalDate.of(2022, 8, 10));
+
+        //when
+        LocalDate expirationDate = LocalDate.of(2023, 2, 28);
+        service.updateChequeValidity(meal, expirationDate);
+
+        //then
+        assertThat(meal.isValid()).isTrue();
+    }
+
+    @Test
+    void meal_is_still_valid_the_day_of_the_expiration_date_when_non_leap_year() {
+        //given
+        ControlCheques service = new ControlCheques();
+        Meal meal = new Meal(100, LocalDate.of(2023, 8, 10));
+
+        //when
+        LocalDate expirationDate = LocalDate.of(2024, 2, 29);
+        service.updateChequeValidity(meal, expirationDate);
+
+        //then
+        assertThat(meal.isValid()).isTrue();
     }
 }
