98818adeb1cff9d67bd9eceae66b34ac50cf438a
Racim CHEKIRINE <racim.chekirine@edu.esiee.fr>
2022-08-10 17:50:00 +0200

    Creating tests to control Meal expiration date

--- backend-core/src/main/java/com/glady/entity/Meal.java
+++ backend-core/src/main/java/com/glady/entity/Meal.java
@@ -1,10 +1,19 @@
 package com.glady.entity;
 
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 
 public class Meal extends Cheque {
 
     public Meal(final double amount, final LocalDate receptionDate) {
         super(amount,receptionDate);
+        calculateExpirationDate();
+    }
+
+    private void calculateExpirationDate() {
+        int currentYear = super.getReceptionDate().getYear();
+        LocalDate lastDayOfNextFebruary;
+        lastDayOfNextFebruary = LocalDate.of(currentYear + 1, 2, 1).with(TemporalAdjusters.lastDayOfMonth());
+        super.expirationDate = lastDayOfNextFebruary;
     }
 }
--- backend-core/src/test/java/com/glady/entity/MealTest.java
+++ backend-core/src/test/java/com/glady/entity/MealTest.java
@@ -16,4 +16,30 @@ public class MealTest {
         //then
         assertThat(meal.isValid()).isTrue();
     }
+
+    @Test
+    void meal_expiration_date_is_28th_when_non_leap_year_following_the_reception_date() {
+        //given
+        LocalDate receptionDate = LocalDate.of(2022, 8, 10);
+        Meal meal = new Meal(100, receptionDate);
+
+        //when
+        LocalDate expectedExpirationDate = LocalDate.of(2023,2,28);
+
+        //then
+        assertThat(meal.getExpirationDate().equals(expectedExpirationDate)).isTrue();
+    }
+
+    @Test
+    void meal_expiration_date_is_29th_when_leap_year_following_the_reception_date() {
+        //given
+        LocalDate receptionDate = LocalDate.of(2023, 8, 10);
+        Meal meal = new Meal(100, receptionDate);
+
+        //when
+        LocalDate expectedExpirationDate = LocalDate.of(2024,2,29);
+
+        //then
+        assertThat(meal.getExpirationDate().equals(expectedExpirationDate)).isTrue();
+    }
 }
